600ebd243dddf825f4a7892766dafc48047f1c83
Exoduz85 <47660094+Exoduz85@users.noreply.github.com>
2021-06-06 21:13:33 +0200

    adds random numbers to correct project

--- Random/RandomNumbers/RandomNumbers.cpp
+++ Random/RandomNumbers/RandomNumbers.cpp
@@ -2,15 +2,18 @@
 #include <chrono>
 using namespace std;
 using namespace chrono;
+int iterations = 1000000;
+int options = 20;
 
 void printOutRndNumb(){
     int arr[20]{};
-    for(int i = 0; i < 1000000; i++){
+    for(int i = 0; i < iterations; i++){
         int r = (rand() % 20) + 1; // rand() % 20 = 0-19, (rand() % 20) + 1 = 1-20
         arr[r - 1] += 1; // r-1 to not get out of bounds
     }
     for(int i = 0; i < 20; i++){
-	    cout << "Number " << i + 1 << " occurred: " << arr[i] << "times" << endl;
+        const float percentage = (static_cast<float>(arr[i]) / iterations) * 100;
+	    cout << "Number " << i + 1 << " occurred: " << arr[i] << " times ("<< percentage << "%)" << endl;
     }
     cout << std::endl;
 }
